4f09ef28236db4055543ae69169e13d9aed20e64
amtommy <amtommy6@gmail.com>
2023-03-25 13:25:19 +0000

    Add 0128_Longest_Consecutive_Sequence

---
+++ 0128_Longest_Consecutive_Sequence/hashset/longest_consecutive_sequence.cpp
@@ -0,0 +1,20 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> set(nums.begin(), nums.end()); //S: O(N), T: O(N)
+        int curr = 0; int res = 0;
+        for(int i = 0; i < nums.size(); i++){ // T: O(N)
+            if(set.find(nums[i]-1) == set.end()){
+                curr++;
+                while(set.find(nums[i] + curr) != set.end()){
+                    curr++;
+                }
+            }
+            res = max(res, curr);
+            curr = 0;
+        }
+        return res;
+    }
+};
