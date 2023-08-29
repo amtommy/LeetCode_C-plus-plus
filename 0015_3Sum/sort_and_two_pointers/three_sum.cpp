b5c351628a186f7ef6b199012813fa22d808534e
amtommy <amtommy6@gmail.com>
2023-08-29 08:16:51 +0000

    Use the std::set to remove duplicate triplets

--- 0015_3Sum/sort_and_two_pointers/three_sum.cpp
+++ 0015_3Sum/sort_and_two_pointers/three_sum.cpp
@@ -5,19 +5,16 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int vector_size = nums.size();
-        vector<vector<int>> output;
-        sort(nums.begin(), nums.end()); // T: O(nlogn)
-        for(int i = 1; i < vector_size-1; i++){  // T: O(n)
+        vector<vector<int>> output; // S: O(N)
+        set<vector<int>> vector_set; // S: O(N)
+        sort(nums.begin(), nums.end()); // T: O(NlogN)
+        for(int i = 1; i < vector_size-1; i++){ // T: O(N)
             int left_pointer = 0;
             int right_pointer = vector_size - 1;
-            while(left_pointer < i && i < right_pointer){ // T: O(m)
+            while(left_pointer < i && i < right_pointer){ // T: O(N)
                 int sum = nums[left_pointer] + nums[i] + nums[right_pointer];
                 if( sum == 0){
-                    vector<int> set;
-                    set.push_back(nums[left_pointer]);
-                    set.push_back(nums[i]);
-                    set.push_back(nums[right_pointer]);
-                    output.push_back(set);
+                    vector_set.insert({nums[left_pointer], nums[i], nums[right_pointer]});
                     left_pointer++;
                     right_pointer--;
                 }
@@ -28,6 +25,9 @@ public:
                     left_pointer++;
                 }
             }
+        }
+        for(auto element: vector_set){ // T: O(N)
+            output.push_back(element);
         }
         return output;
     }
